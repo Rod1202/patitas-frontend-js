68b7d4bbcf589df7ae4094397ed0a53474ac312c
Rodrigo Carbonel <rcarbonel4471@gmail.com>
2024-10-15 22:37:43 -0500

    primer commit

--- js/inicio.js
+++ js/inicio.js
@@ -6,80 +6,96 @@
  */
 window.addEventListener('load', function(){
     
-    // referenciar controles del formulario
+    //referenciar los controles del formulario
     const tipoDocumento = this.document.getElementById('tipoDocumento');
     const numeroDocumento = this.document.getElementById('numeroDocumento');
     const password = this.document.getElementById('password');
     const btnIngresar = this.document.getElementById('btnIngresar');
     const msgError = this.document.getElementById('msgError');
+    const msgSucces = this.document.getElementById('msgSucces');
+    const resultLogout = JSON.parse(this.localStorage.getItem('resultLogout'));
+    const msgLogout = document.getElementById('msgLogout');
 
-    // implementar listener del boton
-    btnIngresar.addEventListener('click', function(){
+    // Recuperar el mensaje de cierre de sesión de localStorage
+    const logoutMessage = localStorage.getItem('logoutMessage');
+    if (logoutMessage) {
+        // Mostrar el mensaje
+        msgLogout.innerHTML = logoutMessage;
+        msgLogout.style.display = 'block';
+       
+    }
 
-        // validar campos del formulario
-        if(tipoDocumento.value === null || tipoDocumento.value.trim() === '' || 
-            numeroDocumento.value === null || numeroDocumento.value.trim() === '' || 
-            password.value === null || password.value.trim() === '') {
+
+    if (resultLogout) {
+        mostrarAlerta(`${resultLogout.mensajeError}`);
+    }
+   
+
+    //implementar listener del boton
+    btnIngresar.addEventListener('click',function(){
+        //validar campos del formulario
+        if(tipoDocumento.value === null || tipoDocumento.value.trim() === ''||
+            numeroDocumento.value === null || numeroDocumento.value.trim() === ''||
+            password.value === null || password.value.trim() === '' ){
                 mostrarAlerta('Error: Debe completar correctamente sus credenciales');
+                
                 return;
-        }
-        ocultarAlerta();
-        autenticar();
-
+            }
+            ocultarAlerta();
+            autenticar();
     });
 
 });
 
-function mostrarAlerta(mensaje) {
+function mostrarAlerta(mensaje){
     msgError.innerHTML = mensaje;
-    msgError.style.display = 'block';
+     msgError.style.display = 'block';
+     setTimeout(() => {
+        ocultarAlerta();
+    }, 3000);
 }
-
-function ocultarAlerta() {
+function ocultarAlerta(mensaje){
     msgError.innerHTML = '';
-    msgError.style.display = 'none';
+     msgError.style.display = 'none';
 }
-
-async function autenticar() {
+async function autenticar(){
 
     const url = 'http://localhost:8082/login/autenticar-async';
     const request = {
-        tipoDocumento: tipoDocumento.value,
+        tipoDocumento : tipoDocumento.value,
         numeroDocumento: numeroDocumento.value,
         password: password.value
     };
 
-    try {
-        
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(request)
-        });
-
-        if(!response.ok) {
-            mostrarAlerta('Error: Ocurrió un problema con la autenticación');
-            throw new Error(`Error: ${response.statusText}`);
-        }
-
-        // validar respuesta
-        const result = await response.json();
-        console.log('Respuesta del servidor: ', result);
-
-        if(result.codigo === '00') {
-            localStorage.setItem('result', JSON.stringify(result));
-            window.location.replace('principal.html');
-        } else {
-            mostrarAlerta(result.mensaje);
-        }
-
-    } catch (error) {
-        
-        console.log('Error: Ocurrió un problema', error);
-        mostrarAlerta('Error: Ocurrió un problema');
-
+    try{
+        const response = await fetch(url,{
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(request)
+    });
+    if(!response.ok){
+        mostrarAlerta('Error: Ocurrio un problema con la autenticacion');
+        throw new Error(`Error: ${response.statusText}`);
     }
+    //validar respuesta
+    const result = await response.json();
+    console.log('Respuesta del servidor: ', result);
 
+    if(result.codigo === '00'){
+        localStorage.setItem('result',JSON.stringify(result));
+        localStorage.setItem('tipoDocumento', tipoDocumento.value);
+        localStorage.setItem('numeroDocumento', numeroDocumento.value); 
+        localStorage.removeItem('resultLogout');
+        window.location.replace('principal.html');
+    }else{
+        mostrarAlerta(result.mensaje);
+    }
+        
+
+    }catch(error){
+        console.log('Error: Ocurrió un problema ', error);
+        mostrarAlerta('Error: Ocurrio un problema ')
+    }
 }
--- js/principal.js
+++ js/principal.js
@@ -1,17 +1,89 @@
-window.addEventListener('load', function(){
-
-    // referenciar controles de pantalla
-    const msgSuccess = this.document.getElementById('msgSuccess');
-
-    // recuperar nombre de usuario
+window.addEventListener('load', function () {
+    const msgSucces = this.document.getElementById('msgSucces');
+    const msgError = this.document.getElementById('msgError');
     const result = JSON.parse(this.localStorage.getItem('result'));
+    const btnLogout = this.document.getElementById('btnCerrarSesion');
 
-    // mostrar nombre de usuario en alerta
-    mostrarAlerta(`Bienvenido ${result.nombreUsuario}`);
+    // Mostrar nombre de usuario en alerta
+    if (result && result.nombreUsuario) {
+        mostrarAlerta(`¡Hola, ${result.nombreUsuario}! Bienvenido de nuevo.`, msgSucces);
+    } else {
+        mostrarError("Lo sentimos, no se encontró información de usuario. Por favor, inicie sesión.", msgError);
+    }
 
+    btnLogout.addEventListener('click', function (event) {
+        event.preventDefault();  
+        logout();  
+    });
 });
 
-function mostrarAlerta(mensaje) {
-    msgSuccess.innerHTML = mensaje;
-    msgSuccess.style.display = 'block';
+function mostrarError(mensaje, element) {
+    element.innerHTML = mensaje;
+    element.style.display = 'block';
+}
+
+function mostrarAlerta(mensaje, element) {
+    element.innerHTML = mensaje;
+    element.style.display = 'block';
+}
+
+function ocultarAlerta(element) {
+    element.innerHTML = '';
+    element.style.display = 'none';
+}
+
+async function logout() {
+    const url = 'http://localhost:8082/login/cerrar-sesion-async';
+    const tipoDocumento = localStorage.getItem('tipoDocumento');
+    const numeroDocumento = localStorage.getItem('numeroDocumento');
+
+    // Verificar si se encontraron los valores necesarios
+    if (!tipoDocumento || !numeroDocumento) {
+        console.error("No se encontraron datos válidos en localStorage.");
+        mostrarError("Error: No se encontraron datos válidos para cerrar sesión. Por favor, inténtelo de nuevo.",
+             document.getElementById('msgError'));
+        return;
+    }
+
+    const responseBody = {
+        tipoDocumento: tipoDocumento,
+        numeroDocumento: numeroDocumento
+    };
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(responseBody)
+        });
+
+        
+        if (!response.ok) {
+            const errorMessage = await response.text(); 
+            console.error('Error al cerrar sesión: ', errorMessage);
+            mostrarError(`${errorMessage}. Por favor, inténtelo de nuevo.`, document.getElementById('msgError'));
+            return;
+        }
+
+ 
+        const resultLogout = await response.json();
+        console.log('Respuesta del servidor: ', resultLogout);
+
+        
+        if (resultLogout.errorMessage === "Exito") {
+            
+            localStorage.removeItem('result');
+            localStorage.removeItem('tipoDocumento');
+            localStorage.removeItem('numeroDocumento');
+           
+            localStorage.setItem('logoutMessage', "¡Cierre de sesión realizado correctamente! Hasta pronto.");
+            window.location.replace('index.html'); 
+            mostrarError(`Error: ${resultLogout.errorMessage}. Inténtelo nuevamente.`, document.getElementById('msgError'));
+        }
+    } catch (error) {
+        console.error('Error: Ocurrió un problema ', error);
+        mostrarAlerta('Error: Ocurrió un problema al cerrar sesión', document.getElementById('msgSucces'));
+    }
 }
